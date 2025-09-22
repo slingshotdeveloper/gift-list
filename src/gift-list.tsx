29c6faf6907c3295e326d837886704edabceafd9
slingshotdeveloper <slingshotdevelop@gmail.com>
2025-09-22 15:15:31 -0500

    edited mobile header

--- src/gift-list.tsx
+++ src/gift-list.tsx
@@ -4,6 +4,11 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './gift-list.less';
 
+const month = new Date().getMonth(); // 0 = Jan, 11 = Dec
+const isChristmas = month === 10 || month === 11; // Nov & Dec
+
+document.body.classList.add(isChristmas ? 'christmas-theme' : 'party-theme');
+
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
